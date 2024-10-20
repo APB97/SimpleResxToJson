5b83511150e3de11cf048444e090a8575b135305
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2024-10-20 22:02:35 +0200

    Add ProcessMultipleFiles

--- SimpleResxToJson.CLI/Program.cs
+++ SimpleResxToJson.CLI/Program.cs
@@ -10,12 +10,45 @@ if (inputs.Length > 0)
     {
         outputPath = output[0].Replace("--output=", string.Empty);
     }
-    foreach (var input in inputs)
+
+    await ProcessInputs(inputs, outputPath);
+}
+
+static async Task ProcessInputs(ImmutableArray<string> inputs, string? outputPath)
+{
+    foreach (var inputPath in from input in inputs
+                              let inputPath = input.Replace("--input=", string.Empty)
+                              select inputPath)
     {
-        var inputPath = input.Replace("--input=", string.Empty);
-        outputPath ??= Path.Combine(Path.GetDirectoryName(inputPath) ?? Directory.GetCurrentDirectory(), $"{Path.GetFileNameWithoutExtension(inputPath)}.json");
-        Console.WriteLine("Converting {0} into {1} ...", inputPath, outputPath);
-        await SingleResxConverter.ConvertFileAsync(inputPath, outputPath);
-        Console.WriteLine("Converted {0} into {1}", inputPath, outputPath);
+        if (Directory.Exists(inputPath))
+        {
+            await ProcessMultipleFiles(outputPath, inputPath);
+        }
+        else
+        {
+            await ProcessSingleFile(outputPath, inputPath, Path.GetDirectoryName(inputPath));
+        }
     }
+}
+
+static async Task ProcessMultipleFiles(string? outputPath, string inputPath)
+{
+    foreach (var file in Directory.EnumerateFiles(inputPath, "*.resx", SearchOption.AllDirectories))
+    {
+        await ProcessSingleFile(outputPath, file, inputPath);
+    }
+}
+
+static async Task ProcessSingleFile(string? outputPath, string inputPath, string? inputDirectory)
+{
+    var inputPathParentDirectory = Path.GetDirectoryName(inputPath);
+    outputPath ??= Path.Combine(
+        inputDirectory == null || inputPathParentDirectory == null
+            ? Directory.GetCurrentDirectory()
+            : Path.Combine(Directory.GetCurrentDirectory(), Path.GetRelativePath(inputDirectory, inputPathParentDirectory)),
+        $"{Path.GetFileNameWithoutExtension(inputPath)}.json");
+
+    Console.WriteLine("Converting {0} into {1} ...", inputPath, outputPath);
+    await SingleResxConverter.ConvertFileAsync(inputPath, outputPath);
+    Console.WriteLine("Converted {0} into {1}", inputPath, outputPath);
 }
