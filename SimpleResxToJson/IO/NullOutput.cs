dd542b3f398d6be51c13f2c8e1f5fcd2a1ae6354
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2024-10-23 16:45:10 +0200

    Document NullOutput class

--- SimpleResxToJson/IO/NullOutput.cs
+++ SimpleResxToJson/IO/NullOutput.cs
@@ -1,13 +1,19 @@
 ﻿namespace apb97.github.io.SimpleResxToJson.Shared.IO;
 
+/// <summary>
+/// Null Object pattern for <see cref="IOutput"/> interface.
+/// </summary>
 public sealed class NullOutput : IOutput
 {
     private NullOutput() { }
 
     /// <summary>
-    /// As it is a NullOutput method, it simply does nothing
+    /// As it is a <see cref="NullOutput"/> method, it simply does nothing and ignores all parameters.
     /// </summary>
     public void PrintMessage(string message, params object[] parameters) { }
 
+    /// <summary>
+    /// An instance of <see cref="NullOutput"/>.
+    /// </summary>
     public static readonly NullOutput Instance = new();
 }
