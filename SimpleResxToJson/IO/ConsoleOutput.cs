bf13ee71a72a511055c52aaf3a65e6fe48d7e5d8
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2024-10-23 16:48:33 +0200

    Document ConsoleOutput class

--- SimpleResxToJson/IO/ConsoleOutput.cs
+++ SimpleResxToJson/IO/ConsoleOutput.cs
@@ -1,7 +1,15 @@
 ﻿namespace apb97.github.io.SimpleResxToJson.Shared.IO;
 
+/// <summary>
+/// Console Output class for <see cref="IOutput"/> interface.
+/// </summary>
 public class ConsoleOutput : IOutput
 {
+    /// <summary>
+    /// Forwads parameters to <see cref="Console.WriteLine(string, object?[]?)"/> for printing to console
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="parameters"></param>
     public void PrintMessage(string message, params object[] parameters)
     {
         Console.WriteLine(message, parameters);
